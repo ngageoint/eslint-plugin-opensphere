e499e72d52656d988f960dd2407f184b763af438
William Wall <wwall@caci.com>
2018-02-15 14:29:44 -0700

    docs(function): we should probably follow our own rules some

--- require-jsdoc.js
+++ require-jsdoc.js
@@ -8,27 +8,39 @@
  *     // ...
  *   };
  */
-"use strict";
+'use strict';
 
 var original = require('eslint/lib/rules/require-jsdoc');
 
 // just extend the original rule
 var originalCreate = original.create;
 
+/**
+ * Overrides the original rule to always check for JSDoc on FunctionExpressions
+ * within AssignmentExpressions.
+ *
+ * @override
+ */
 original.create = function(context) {
   var rule = originalCreate(context);
   var check = rule.FunctionDeclaration;
   var old = rule.FunctionExpression;
 
+  /**
+   * Overrides the original rule to always check for JSDoc on FunctionExpressions
+   * within AssignmentExpressions.
+   *
+   * @override
+   */
   rule.FunctionExpression = function(node) {
     if (node.parent.type === 'AssignmentExpression') {
       check(node);
     }
 
     old(node);
-  }
+  };
 
   return rule;
-}
+};
 
 exports.rule = original;
