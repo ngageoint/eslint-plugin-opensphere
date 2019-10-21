8580945a5680ef3e08ad37c5e3de4b3bd8cbd014
Kevin Schmidt <kschmidt@caci.com>
2019-10-21 08:08:02 -0600

    feat(goog): allow goog.module statements to precede goog.require

--- requires-first.js
+++ requires-first.js
@@ -5,7 +5,7 @@ const util = require('./util');
 exports.rule = {
   meta: {
     docs: {
-      description: 'require that all goog.require() precede other statements (except goog.provide())',
+      description: 'require that all goog.require() precede other statements (except goog.provide() and goog.module())',
       category: 'Closure Compiler'
     }
   },
@@ -20,7 +20,7 @@ exports.rule = {
             if (otherSeen) {
               return context.report(statement, 'Expected goog.require() to precede other statements');
             }
-          } else if (!util.isProvideStatement(statement)) {
+          } else if (!util.isProvideStatement(statement) && !util.isModuleStatement(statement)) {
             otherSeen = true;
           }
         });
